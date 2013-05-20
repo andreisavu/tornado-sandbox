b6f7e36d2c305aee13e2ad66239af00bc41b30e7
Andrei Savu <asavu@apache.org>
2013-05-20 12:29:56 +0300

    Set max_clients to allow a large number of concurrent requests

--- simple.py
+++ simple.py
@@ -13,16 +13,16 @@ def handle_request(response):
   if response.error:
     print "Error: %s\n" % response.error
   else:
-    print "%r\n" % response
+    print "%s: %r" % (count, response)
 
   count += 1
-  if count == 100:
+  if count == 5000:
     io.stop()    
 
 AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
-client = AsyncHTTPClient()
+client = AsyncHTTPClient(max_clients = 1000)
 
-for _ in range(0, 100):
+for _ in range(0, 5000):
   client.fetch("http://localhost:8080/", handle_request)
 
 io.start()
